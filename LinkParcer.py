745085b5fe66be04d19dfd0cdb1a9939118f6679
sci-web <sci-web@protonmail.com>
2017-12-21 15:24:48 +0100

    Parser updated, jsons

--- LinkParcer.py
+++ LinkParcer.py
@@ -1,12 +1,18 @@
 import getopt, re, sys
 from BeautifulSoup import BeautifulSoup
 
+
 def parse(html):
+    num = html[0:html.find(".")]
     digest = html[html.find("<br>"):]
     digest = digest[4:]
     soup = BeautifulSoup(html)
-    title = soup.a.string
-    link = soup.a.href
+    try:
+        title = soup.a.string
+        link = soup.a.href
+    except:
+        title = ""
+        link = ""
     pr_info = title[title.rfind("(")+1:title.rfind(")")]
     dt = ["","",""]
     if title.find("(") > 0:
@@ -20,22 +26,25 @@ def parse(html):
     inner = BeautifulSoup(digest)
     pattern =r'<(a|/a).*?>'    
     for a in inner.findAll('a', href=True):
-        lnk = "[" + a.string + "==" + a['href'] + "]"
-        # print lnk.encode('utf-8')
-        # print str(a)
-        digest = re.sub(pattern, "*", digest)
-        digest = digest.replace("*"+str(a.string)+"*", lnk.encode('utf-8'))
-    print "{"
-    print "\"Title\": \"" + title + "\","
-    print "\"Link\": \"" + link + "\","
-    print "\"InfoID\": \"article\","
-    print "\"InfoDate\": \"" + dt[1] + "\","
-    print "\"InfoAuthors\": \"" + dt[0] + "\","
-    print "\"InfoID\": \"\","
-    print "\"InfoPlace\": \"" + dt[2] + "\","
-    print "\"Digest\": \"" + digest + "\""
-    print "},"
-    
+        try:
+            lnk = "[" + a.string + "==" + a['href'] + "]"
+            digest = re.sub(pattern, "*", digest)
+            digest = digest.replace("*"+str(a.string)+"*", lnk.encode('utf-8'))
+        except: 
+            print a.string
+    print "    {"
+    print "        \"Num\": " + num + ","    
+    print "        \"Title\": \"" + title + "\","
+    print "        \"Link\": \"" + link + "\","
+    print "        \"InfoType\": \"article\","
+    print "        \"InfoDate\": \"" + dt[1] + "\","
+    print "        \"InfoAuthors\": \"" + dt[0] + "\","
+    print "        \"InfoID\": \"\","
+    print "        \"InfoPlace\": \"" + dt[2] + "\","
+    print "        \"Digest\": \"" + digest + "\""
+    print "    },"
+
+
 def main(argv):
     try:
         opts, arg = getopt.getopt(argv, "t:", ["txt="])
@@ -44,13 +53,15 @@ def main(argv):
     if len(opts) > 0:
         for opt, arg in opts:
             if opt in ("-t", "--txt") and arg:
-                arg.rstrip("\n\r")    
+                arg.rstrip("\n\r")
                 parse(arg)
             else:
                 print "no HTML input"
     else:
         for point in sys.stdin.read().split("\n\n"):
+            point.rstrip("\n\r") 
             parse(point)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])

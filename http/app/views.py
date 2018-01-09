ee5a71f8bcdce1c5fa7b9b689a9080a61d2964c3
sci-web <sci-web@protonmail.com>
2018-01-09 15:21:01 +0100

    content management interfaces started

--- http/app/views.py
+++ http/app/views.py
@@ -18,6 +18,7 @@ def load_vars():
     g.drugs = DB().get_objects_by_key_sorted_filter_yes("drug", "I_S_name")
     g.conditions = DB().get_objects_by_key_sorted_filter_yes("condition", "I_S_name")    
     g.objects_geo = DB().get_objects_by_key_sorted_filter_yes("geo", "I_S_name")
+    g.chapters = DB().get_spaces_by_key_sorted("vaccines", "date")
     g.form = makeform()
 
 def date_format(view, value):
@@ -39,7 +40,7 @@ app.jinja_env.filters['rehref'] = rehref
 @app.route('/')
 def index():
     i_data = DB().get_intros()
-    return render_template('index.html', form=g.form, items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, data=i_data)
+    return render_template('index.html', form=g.form, items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=g.chapters, data=i_data)
 
 
 @app.route('/content/<namespace>/<codename>')
@@ -51,13 +52,12 @@ def show_item(namespace, codename):
     f_date = datetime.datetime.fromtimestamp( data[0]["date"]['$date'] / 1e3 )
     data[0]["date"] = f_date.strftime('%d-%m-%Y %H:%M')    
     return render_template('content.html', idata=data, form=g.form, items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, 
-                            title=namespace.title() + ": " + title, chapters=chapters)
+                            title=namespace.title() + ": " + title, chapters=g.chapters)
 
 
 @app.route('/search', methods=['GET','POST'])
 def search():
     i_data = {}
-    chapters = DB().get_spaces_by_key_sorted("vaccines", "date")
     searchfor = ""
     sform = searchForm(request.values)
     fdata = []  # filtered data    
@@ -86,28 +86,26 @@ def search():
     else:
         flash("Form was not properly sent", category='error')
     return render_template('srp.html', idata=fdata, found=len(fdata),
-            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=chapters, title=searchfor)
+            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=g.chapters, title=searchfor)
 
 
 
 @app.route('/browse/obj/<codename>')
 def browse(codename):
     i_data = DB().get_points_by_codename(codename)
-    chapters = DB().get_spaces_by_key_sorted("vaccines", "date")
     obj = DB().get_an_object_by_codename(codename)
     objdata = json.loads(dumps(obj))
     return render_template('browse.html', idata=i_data, obj=objdata,
-            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=chapters, title=objdata[0]["I_S_name"])
+            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=g.chapters, title=objdata[0]["I_S_name"])
 
 
 @app.route('/browse/geo/<geo>')
 def browse_geo(geo):
     i_data = DB().get_points_by_geo(geo)
-    chapters = DB().get_spaces_by_key_sorted("vaccines", "date")
     obj = DB().get_an_object_by_codename(geo)
     objdata = json.loads(dumps(obj))
     return render_template('browse.html', idata=i_data, obj=objdata,
-            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=chapters, title=objdata[0]["I_S_name"])
+            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=g.chapters, title=objdata[0]["I_S_name"])
 
 
 @app.route('/intro/<namespace>',methods=['GET','POST'])
@@ -116,12 +114,11 @@ def show_intro(namespace):
     data = json.loads(dumps(i_data))
     f_date = datetime.datetime.fromtimestamp( data[0]["date"]['$date'] / 1e3 )
     data[0]["date"] = f_date.strftime('%d-%m-%Y %H:%M')
-    chapters = DB().get_spaces_by_key_sorted("vaccines", "date")
     if 'div' in request.args:
-        return jsonify( {'data': render_template('intro_div.html', idata=data)} )
+        return jsonify( {'data': render_template('intro_div.html', idata=data, chapters=g.chapters)} )
     else:
         return render_template('intro.html', idata=data, 
-            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=chapters, title=data[0]["subject"])
+            items=g.items, objects=g.objects, conditions=g.conditions, drugs=g.drugs, geo_objects=g.objects_geo, chapters=g.chapters, title=data[0]["subject"])
 
 
 @app.route('/chapters/<namespace>',methods=['GET','POST'])

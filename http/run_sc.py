31589c51e99dc4bb78e42d8eed21446be05b7cb9
j_cth <catherine.kirsanova@axpm.com>
2018-12-04 13:52:08 +0300

    new skin run py

---
+++ http/run_sc.py
@@ -0,0 +1,16 @@
+from sc import app, lm
+import os
+import logging
+
+logging.basicConfig(filename='access.log', level=logging.DEBUG)
+watch_dirs = ['sc/templates']
+watch_files = watch_dirs[:]
+for w_dir in watch_dirs:
+    for dirname, dirs, files in os.walk(w_dir):
+        for filename in files:
+            filename = os.path.join(dirname, filename)
+            if os.path.isfile(filename):
+                watch_files.append(filename)
+app.run('0.0.0.0', port=8080, extra_files=watch_files, threaded=True)
+app.debug = True
+# app.config['DEBUG'] = True
